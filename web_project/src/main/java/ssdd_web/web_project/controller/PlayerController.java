2982fab7104712e78c4d7aeef2cd38220cbd5c33
nacho <nacho.carnal@gmail.com>
2025-02-23 13:07:32 +0100

    created team controller nad team service

--- web_project/src/main/java/ssdd_web/web_project/controller/PlayerController.java
+++ web_project/src/main/java/ssdd_web/web_project/controller/PlayerController.java
@@ -8,9 +8,7 @@ import ssdd_web.web_project.model.Player;
 import ssdd_web.web_project.services.PlayerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class PlayerController {
--- web_project/src/main/java/ssdd_web/web_project/services/TeamService.java
+++ web_project/src/main/java/ssdd_web/web_project/services/TeamService.java
@@ -1,8 +1,43 @@
 package ssdd_web.web_project.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ssdd_web.web_project.model.Player;
+import ssdd_web.web_project.model.Team;
+import ssdd_web.web_project.repository.PlayerRepository;
+import ssdd_web.web_project.repository.TeamRepository;
 
 @Service
 public class TeamService {
 
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    // all players list
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public Team createTeam(Long player1Id, Long player2Id) {
+        Optional<Player> player1 = playerRepository.findById(player1Id);
+        Optional<Player> player2 = playerRepository.findById(player2Id);
+
+        if (player1.isPresent() && player2.isPresent()) {
+            Team team = new Team(player1.get(), player2.get());
+            return teamRepository.save(team);
+        }
+        throw new RuntimeException("Jugador no encontrado");
+    }
+
+    // get team by id
+    public Optional<Team> getTeamById(Long id) {
+        return teamRepository.findById(id);
+    }
 }
