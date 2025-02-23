4e1e868442da8567d56556b4cfc389da93a9bc7b
nacho <nacho.carnal@gmail.com>
2025-02-23 12:21:58 +0100

    player controller and player service created

--- web_project/src/main/java/ssdd_web/web_project/controller/PlayerController.java
+++ web_project/src/main/java/ssdd_web/web_project/controller/PlayerController.java
@@ -1,8 +1,35 @@
 package ssdd_web.web_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import ssdd_web.web_project.model.Player;
+import ssdd_web.web_project.services.PlayerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    // new player
+    @GetMapping("/players/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("player", new Player());
+        return "PlayerRegistration"; // Thymeleaf buscara "PlayerRegistration.html"
+    }
+
+    // save player in database
+    @PostMapping("/players/add")
+    public String postMethodName(@ModelAttribute Player player) {
+        playerService.savePlayer(player);
+        return "redirect:/players/" + player.getId() + "/stats";
+    }
 
 }
--- web_project/src/main/java/ssdd_web/web_project/services/PlayerService.java
+++ web_project/src/main/java/ssdd_web/web_project/services/PlayerService.java
@@ -1,8 +1,26 @@
 package ssdd_web.web_project.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ssdd_web.web_project.model.Player;
+import ssdd_web.web_project.repository.PlayerRepository;
 
 @Service
 public class PlayerService {
 
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    // save player in database
+    public Player savePlayer(Player player) {
+        return playerRepository.save(player);
+    }
+
+    // get player by id
+    public Optional<Player> getPlayerById(Long id) {
+        return playerRepository.findById(id);
+    }
 }
