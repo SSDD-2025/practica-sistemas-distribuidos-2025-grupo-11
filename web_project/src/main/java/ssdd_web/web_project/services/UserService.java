fc34cb4717a4d9c57bac80d498a840e7e6ec25ac
sgiker21 <iker.moranteaceituno04@gmail.com>
2025-03-02 20:25:35 +0100

    Added error page. Added login for users. Added user stored in the database (some details left)

--- web_project/src/main/java/ssdd_web/web_project/services/UserService.java
+++ web_project/src/main/java/ssdd_web/web_project/services/UserService.java
@@ -1,8 +1,18 @@
 package ssdd_web.web_project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ssdd_web.web_project.model.User;
+import ssdd_web.web_project.repository.UserRepository;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+    // save user to database
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
