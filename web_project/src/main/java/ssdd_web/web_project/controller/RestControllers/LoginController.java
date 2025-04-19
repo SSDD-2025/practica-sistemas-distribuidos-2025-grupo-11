c0f01310ed7c827595cc3ed79a9f0c8fb4136032
nacho <nacho.carnal@gmail.com>
2025-04-19 10:48:41 +0200

    added several security functionalities

---
+++ web_project/src/main/java/ssdd_web/web_project/controller/RestControllers/LoginController.java
@@ -0,0 +1,41 @@
+package ssdd_web.web_project.controller.RestControllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+import ssdd_web.web_project.security.jwt.AuthResponse;
+import ssdd_web.web_project.security.jwt.AuthResponse.Status;
+import ssdd_web.web_project.security.jwt.LoginRequest;
+import ssdd_web.web_project.security.jwt.UserLoginService;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/auth")
+public class LoginController {
+
+    @Autowired
+    private UserLoginService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        return userService.login(response, loginRequest);
+    }
+
+    @PostMapping("path")
+    public ResponseEntity<AuthResponse> refreshToken(
+            @CookieValue(name = "RefreshToken", required = false) String refreshToken, HttpServletResponse response) {
+        return userService.refresh(response, refreshToken);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<AuthResponse> logOut(HttpServletResponse response) {
+        return ResponseEntity.ok(new AuthResponse(Status.SUCCESS, userService.logout(response)));
+    }
+
+}
--- web_project/src/main/java/ssdd_web/web_project/security/SecurityConfig.java
+++ web_project/src/main/java/ssdd_web/web_project/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package ssdd_web.web_project.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -22,6 +24,12 @@ public class SecurityConfig {
         @Autowired
         public SecurityConfig(RepositoryUserDetailsService userDetailsService) {
                 this.userDetailsService = userDetailsService;
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+                        throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
         }
 
         @Bean
@@ -55,4 +63,5 @@ public class SecurityConfig {
         public PasswordEncoder passwordEncoder() {
                 return new BCryptPasswordEncoder();
         }
+
 }
