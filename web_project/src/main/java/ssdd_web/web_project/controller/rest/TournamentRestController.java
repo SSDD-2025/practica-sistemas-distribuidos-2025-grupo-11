8b2eb2dd45055a3ddb22f734c37466cc31748b05
Nachooo51 <nacho.carnal@gmail.com>
2025-04-23 12:50:01 +0200

    last minute changes

--- web_project/src/main/java/ssdd_web/web_project/controller/rest/TournamentRestController.java
+++ web_project/src/main/java/ssdd_web/web_project/controller/rest/TournamentRestController.java
@@ -1,22 +1,24 @@
 package ssdd_web.web_project.controller.rest;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import ssdd_web.web_project.DTO.TournamentDTO;
-import ssdd_web.web_project.services.TournamentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import ssdd_web.web_project.DTO.TournamentDTO;
+import ssdd_web.web_project.services.TournamentService;
 
 @RestController
 @RequestMapping("/api/tournaments")
@@ -35,6 +37,10 @@ public class TournamentRestController {
     }
 
     @GetMapping("/{id}")
+    public ResponseEntity<TournamentDTO> getTournamentsById(@PathVariable Long id) {
+        TournamentDTO tournament = tournamentService.getTournamentById(id);
+        return ResponseEntity.ok(tournament);
+    }
 
     @PostMapping
     public ResponseEntity<TournamentDTO> createTournament(@RequestBody TournamentDTO tournamentDTO) {
